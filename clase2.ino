274f75eee890ba6649bbabc4a6d842da9a10da36
NicolasAversa <nicolasaversadm@gmail.com>
2026-05-26 19:43:02 -0300

    Migracion a cm

--- clase2.ino
+++ clase2.ino
@@ -14,13 +14,13 @@ void loop()
     delayMicroseconds(10);
     digitalWrite(TRIG, LOW);
 
-    // 2. Medir distancia directamente en METROS
-    float metros = (pulseIn(ECHO, HIGH) * 0.034 / 2) / 100.0;
+    // 2. Calcular distancia en centímetros (0 a 255 cm)
+    int cms = pulseIn(ECHO, HIGH) * 0.034 / 2;
 
-    // 3. Mapear: 0 a 2 metros -> 0 a 4 segundos de delay (0 a 4000 ms)
-    long velocidad = map(metros * 100, 0, 200, 0, 4000);
+    // 3. Mapear: 0 a 255 cm -> 0 a 5000 milisegundos (0 a 5 segundos)
+    long velocidad = map(cms, 0, 255, 0, 5000);
 
-    // 4. Parpadeo del LED
+    // 4. Parpadeo del LED (Cerca = Delay corto = Parpadeo rápido)
     digitalWrite(LED, HIGH);
     delay(velocidad);
     digitalWrite(LED, LOW);
