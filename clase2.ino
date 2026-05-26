a2e81e269c9ab7d7cdf1e810550dbdb989a8208e
NicolasAversa <nicolasaversadm@gmail.com>
2026-05-26 19:41:53 -0300

    Mas simple

--- clase2.ino
+++ clase2.ino
@@ -1,34 +1,28 @@
-const int PIN_TRIG = 9;
-const int PIN_ECHO = 10;
-const int PIN_LED = 13;
+const int TRIG = 9, ECHO = 10, LED = 13;
 
 void setup()
 {
-    pinMode(PIN_TRIG, OUTPUT);
-    pinMode(PIN_ECHO, INPUT);
-    pinMode(PIN_LED, OUTPUT);
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+    pinMode(LED, OUTPUT);
 }
 
 void loop()
 {
-    // 1. Lanzar el pulso de ultrasonido
-    digitalWrite(PIN_TRIG, LOW);
-    delayMicroseconds(2);
-    digitalWrite(PIN_TRIG, HIGH);
+    // 1. Pulso rápido para activar el sensor
+    digitalWrite(TRIG, HIGH);
     delayMicroseconds(10);
-    digitalWrite(PIN_TRIG, LOW);
+    digitalWrite(TRIG, LOW);
 
-    // 2. Calcular distancia (0 a 255)
-    long duracion = pulseIn(PIN_ECHO, HIGH);
-    int distancia = constrains(duracion * 0.034 / 2, 0, 255);
+    // 2. Medir distancia directamente en METROS
+    float metros = (pulseIn(ECHO, HIGH) * 0.034 / 2) / 100.0;
 
-    // 3. Mapear: Cerca (0) = Espera corta (0ms) -> Rápido
-    //            Lejos (255) = Espera larga (5000ms) -> Lento
-    long velocidad = map(distancia, 0, 255, 0, 5000);
+    // 3. Mapear: 0 a 2 metros -> 0 a 4 segundos de delay (0 a 4000 ms)
+    long velocidad = map(metros * 100, 0, 200, 0, 4000);
 
     // 4. Parpadeo del LED
-    digitalWrite(PIN_LED, HIGH);
+    digitalWrite(LED, HIGH);
     delay(velocidad);
-    digitalWrite(PIN_LED, LOW);
-    delay(velocidad); // Añadido para que se note el tiempo de apagado también
+    digitalWrite(LED, LOW);
+    delay(velocidad);
 }
